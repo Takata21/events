5d1fe7eda5a650f8f6042886276a519c89b31396
Manuel Takata <takata211011@gmail.com>
2024-01-04 14:02:27 -0500

    log2

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -4,7 +4,7 @@ import { createOrder } from "@/lib/actions/order.actions";
 
 export async function POST(request: Request) {
   const body = await request.text();
-  console.log(body);
+  console.log("bodyy----------------------------->>>>>>>>>>>>>>>>>>>>><", body);
   const sig = request.headers.get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
