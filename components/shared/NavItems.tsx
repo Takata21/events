fee63f0144f3303ad319c3f9fd8a30ad3633f989
Manuel Takata <takata211011@gmail.com>
2024-01-04 14:15:05 -0500

    updat code

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,27 +1,31 @@
-"use client";
-import { headerLinks } from "@/constants";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
+'use client';
 
-export default function NavItems() {
+import { headerLinks } from '@/constants'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const NavItems = () => {
   const pathname = usePathname();
 
   return (
-    <ul className="md:flex-between flex w-full flex-col md:flex-row items-start gap-5 md:flex-grow whitespace-nowrap">
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
       {headerLinks.map((link) => {
         const isActive = pathname === link.route;
+        
         return (
           <li
             key={link.route}
             className={`${
-              isActive && "text-primary-500"
-            } flex-center p-medium-16`}
+              isActive && 'text-primary-500'
+            } flex-center p-medium-16 whitespace-nowrap`}
           >
             <Link href={link.route}>{link.label}</Link>
           </li>
-        );
+        )
       })}
     </ul>
-  );
+  )
 }
+
+export default NavItems
